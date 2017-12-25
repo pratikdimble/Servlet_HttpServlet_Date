2be223864fdc6a287f13e1062781f412a771a131
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-24 23:40:31 -0800

    Create DisplayDate.java

---
+++ com/app/DisplayDate.java
@@ -0,0 +1,19 @@
+package com.app;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+
+import javax.servlet.http.*;
+
+public class DisplayDate extends HttpServlet{
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+										throws ServletException, IOException
+	{
+		PrintWriter pw = response.getWriter();
+			Date today = new Date();
+				pw.println("<html>"+"<body><h1>Today Date is</h1>");
+					pw.println("<b>"+ today+"</b></body>"+ "</html>");
+  }
+}
